57f4114aa2210806ea51830ef494c0aa9bd3a6b4
Naveen Kumar K G <nkumarkg@in.ibm.com>
2020-01-10 09:01:35 +0530

    add merge data

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // My First Version
 //I edited this file
+/*
+Brnach_1(Master)           commit _1  Commit -2 commit-3   commit-4 (b)
+
+Branch_2 (development)      commit_1    commit_2 commit-3   Commit-4  (a)
+
+
+git checkout master
+git merge development
+
+
+git checkout b
+git merge a
+
+
+
+*/
