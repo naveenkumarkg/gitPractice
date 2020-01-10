a5a3bcad7aee9b67aa7f0439aee48aded5a1c012
Naveen Kumar K G <nkumarkg@in.ibm.com>
2020-01-10 08:20:55 +0530

    added to development

---
+++ status.js
@@ -0,0 +1,33 @@
+/*
+
+git clone 'Repo address'
+
+git add . || git add file_1 file_2  || using VS Code '+' icon (files to Stage Changes)
+
+git reset || git reset file_1 file_2 || using Vs Code '-' icon (files to unstage)
+
+-----------------------------------------------------------------------
+Basic regular USE
+
+git status
+
+git add .
+
+git status
+
+git commit -m "Add a custom message for commit"
+
+git pull
+
+git push
+
+--------------------------------------------------------------
+Create a Branch Development
+
+git branch 'NEW Branch Name'
+
+
+
+
+
+*/
